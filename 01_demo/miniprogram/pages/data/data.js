c16222d55076b422c157d1086f1c043670b98530
EternityJi <bear@126.com>
2020-05-17 12:49:04 +0800

    小程序demo

---
+++ 01_demo/miniprogram/pages/data/data.js
@@ -0,0 +1,13 @@
+Page({
+  // 小程序和vue一样 数据驱动视图  如果修改页面 修改数据
+  // 小程序中 通过data来提供数据
+  data:{
+    msg:"hello 小程序",
+    user:{
+      name:"zhangsan",
+      age:"14"
+    },
+    imageUrl:"../.././images/note.png",
+    ischeck:true
+  }
+})
